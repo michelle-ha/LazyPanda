f26ba9dc6facc7d45ae8af999f8705e85767e67f
Michelle Ha <michelle1992us@yahoo.com>
2021-02-10 14:49:22 -0800

    fixed error message

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -9,7 +9,7 @@ class Api::SessionsController < ApplicationController
         login(@user)
         render "api/users/show"
       else
-        render json: ["Invalid username and password combination"], status: 401
+        render json: ["Invalid email and password combination"], status: 401
       end
     end
   
