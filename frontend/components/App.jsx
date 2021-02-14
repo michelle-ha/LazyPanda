70fe2313460429ea4cbca4a481cfbc0a345bde43
Michelle Ha <michelle1992us@yahoo.com>
2021-02-13 20:08:13 -0800

    trying to fix redirect issues for false userid

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,6 +4,7 @@ import SignIn from "./greeting/signin"
 import PostFormContainer from "./post/post_form_container"
 import {ProtectedRoute } from '../util/route_util';
 import PostShowContainer from './post/post_show_container'
+import NotFound from "./not_found"
 
 
 
@@ -16,8 +17,8 @@ class App extends React.Component {
         <Switch>
          <Route exact path="/" component={SignIn } />
          <ProtectedRoute exact path="/add-new-post" component={PostFormContainer} />
-         <Route path="/:postId" component={PostShowContainer} />
-         <Route render={() => <Redirect to="/"/>}/>
+         <Route exact path="/:postId" component={PostShowContainer} />
+         <Route path='*' component={NotFound} />
         </Switch>
     )
   }
