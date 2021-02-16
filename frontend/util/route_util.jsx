7db29da40881174b97eb73d9d9a01658c34b72d8
Michelle Ha <michelle1992us@yahoo.com>
2021-02-16 12:20:39 -0800

    commented out unused route method

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 
-const Auth = ({ component: Component, path, loggedIn, exact }) => (
-    <Route
-      path={path}
-      exact={exact}
-      render={props =>
-      !loggedIn ? <Component {...props} /> : <Redirect to="/" />
+// const Auth = ({ component: Component, path, loggedIn, exact }) => (
+//     <Route
+//       path={path}
+//       exact={exact}
+//       render={props =>
+//       !loggedIn ? <Component {...props} /> : <Redirect to="/" />
 
-      }
-    />
-  );
+//       }
+//     />
+//   );
 
 
   
@@ -29,6 +29,6 @@ const Protected = ({ component: Component, path, loggedIn, exact }) => (
     return { loggedIn: Boolean(state.session.id) };
   };
   
-  export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+  // export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
   
   export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
