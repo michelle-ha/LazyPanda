6133812d4696f11c145892cace057d323aa81677
Michelle Ha <michelle1992us@yahoo.com>
2021-03-23 12:10:19 -0700

    getLikes selectors created

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -4,9 +4,9 @@ export const selectPost = ({ posts }, postId) => {
     return posts[postId]
   };
   
-  export const getLikes = (state, item ) => {
+export const getLikes = (state, item ) => {
     let likes = [];
-    const type = item.post_author_id ? 'Post' : 'Subpost';
+    const type = item.subpostIds ? 'Post' : 'Subpost';
 
     Object.values(state.entities.likes).forEach( like => {
         if ( like.likeable_id === item.id && like.likeable_type === type) {
