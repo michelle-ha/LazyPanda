8e6f4820e651cdf9028b0c2496fc7afdb5377a98
Michelle Ha <michelle1992us@yahoo.com>
2021-03-23 18:12:12 -0700

    tables re-migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_215747) do
+ActiveRecord::Schema.define(version: 2021_03_17_014930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2021_02_18_215747) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.string "likeable_type"
+    t.bigint "likeable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable_type_and_likeable_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
@@ -46,13 +55,12 @@ ActiveRecord::Schema.define(version: 2021_02_18_215747) do
     t.index ["title"], name: "index_posts_on_title"
   end
 
-  create_table "sections", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "post_id", null: false
+  create_table "reviews", force: :cascade do |t|
+    t.string "content", null: false
     t.integer "author_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_sections_on_post_id"
   end
 
   create_table "subposts", force: :cascade do |t|
