cbdda449c53f0694f211afa61ff1a214c5de1dae
Michelle Ha <michelle1992us@yahoo.com>
2021-03-25 11:19:08 -0700

    scroll to top added

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import ScrollToTop from "./scrollToTop";
 
 const Root = ({ store }) => (
     <>
         <Provider store={store}>
             <HashRouter>
+                <ScrollToTop />
                 <App />
             </HashRouter>
         </Provider>
