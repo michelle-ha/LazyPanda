90d78856cf8b8cc6b6a5a8f93e141c97a77059e9
Michelle Ha <michelle1992us@yahoo.com>
2021-03-26 00:51:29 -0700

    css to search page

--- frontend/components/post/post_search_fxn.jsx
+++ frontend/components/post/post_search_fxn.jsx
@@ -25,23 +25,42 @@ export const Search = props => {
             }
         }
         let header;
-        if (filterDisplay.length > 0) {
-            header = <h1 className="results">Search Results</h1>
-        } else {header = null}
+        if (filterDisplay.length < 0) {
+            header = "No Results"}
 
         return (
-            <div className="searchResults">
-                <input placeholder="Search for post title..." onChange={e => handleChange(e.target.value)}/>
-                {header}
 
-                {filterDisplay.map((post, i) => (
+            <div>      
+            
+            <div className="picture-edit-container">
+
+                <form className="picture-edit-form" >
+                    <h1 className="profile-pic-heading"> Search For Posts</h1>
+                    <input placeholder="Search for post title..." onChange={e => handleChange(e.target.value)}/>
+                <h3 className="button-holder">Search Results</h3>
+                
+                
+                <h3 className="button-holder">{filterDisplay.map((post, i) => (
+                    filterDisplay.length ? (
                     <div key={i}>
                         <li>
                         <Link to={`/${post.id}`}>{post.title}</Link>
                         <span> {post.author}</span>
                         </li>
-                    </div>
-                ))}
+                    </div>) : ( <span>No Results</span>)
+                ))}</h3>
+                    
+
+                    <div className="go-back-link-edit"><Link to="/" className="go-back">Go Back</Link></div>
+
+
+                </form>
+
+
+                
             </div>
-        )
+            </div>
+        );
     }
+
+           
